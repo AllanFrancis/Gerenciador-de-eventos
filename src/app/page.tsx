7bab0ac5b64d17a4d80f3ef85f49306079967eba
Allan Francis <allantecnologo@gmail.com>
2025-02-20 00:51:51 -0400

    Add Tailwind CSS configuration and implement button and input components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { InputIcon, InputRoot, InputField } from "@/components/input";
+import { CopyIcon, ArrowRight, User2Icon } from "lucide-react";
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="p-4 h-screen space-y-4">
+      <Button>
+        Click me
+        <ArrowRight />
+      </Button>
+      <IconButton>
+        <CopyIcon />
+      </IconButton>
+      <InputRoot>
+        <InputIcon>
+          <User2Icon />
+        </InputIcon>
+        <InputField placeholder="Enter your email" />
+      </InputRoot>
     </main>
   );
 }
