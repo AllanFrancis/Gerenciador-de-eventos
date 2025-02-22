679df583af4c1860848488f9c6e91cc150265727
Allan Francis <allantecnologo@gmail.com>
2025-02-22 12:17:02 -0400

    Refactor homepage layout and move subscription form to a separate component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from '@/components/button'
-import { InputField, InputIcon, InputRoot } from '@/components/input'
-import {
-  ArrowRightIcon,
-  MailIcon,
-  RadioIcon,
-  User,
-  UserIcon,
-} from 'lucide-react'
+import logo from '@/assets/logo.svg'
+import { RadioIcon } from 'lucide-react'
 import Image from 'next/image'
-import logo from '../assets/logo.svg'
+import { SubscriptionForm } from './components/subscription-form'
 
 export default function Home() {
   return (
@@ -44,32 +37,7 @@ export default function Home() {
           </p>
         </div>
 
-        <form className="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]">
-          <h2 className="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-
-          <div className="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <UserIcon />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-
-            <InputRoot>
-              <InputIcon>
-                <MailIcon />
-              </InputIcon>
-              <InputField type="text" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-
-          <Button type="submit">
-            Confirmar
-            <ArrowRightIcon className="size-6" />
-          </Button>
-        </form>
+        <SubscriptionForm />
       </div>
     </div>
   )
